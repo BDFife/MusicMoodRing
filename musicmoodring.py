b327c6ce4924eab063164acd35dbfdd35bbd0511
Brian Fife <bdfife@gmail.com>
2012-11-19 17:33:14 -0500

    Added an about page and modified display code so it shows the same number of moods/themes.

--- musicmoodring.py
+++ musicmoodring.py
@@ -26,13 +26,27 @@ def index():
 #    my_tracks = lfm_get_info(uname, 20)
 #    return str(my_tracks)
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 @app.route('/get/mood/<uname>')
 def get_moods(uname):
     tracks, moods, themes = lfm_get_info(uname, 20)
+    
+    # Trim the longer of the two lists to they match in length.
+    mood_len = len(moods)
+    theme_len = len(themes)
+    
+    if mood_len > theme_len:
+        moods = moods[:theme_len]
+    elif mood_len < theme_len:
+        themes = themes[:mood_len]
+    # if we drop out, they're equal.
+
     return render_template('moods.html', tracks=tracks, moods=moods,
                            themes=themes, user=uname)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-
